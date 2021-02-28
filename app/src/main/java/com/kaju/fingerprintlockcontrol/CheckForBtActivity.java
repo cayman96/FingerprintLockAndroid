d429274faa655b92c4afa2fe67167273b2738446
cayman96 <cayman96@gmail.com>
2021-02-28 19:07:33 +0100

    popup do potwierdzenia usunięcia danego id oraz zachowanieprzycisku wstecz + rozpoczęcie pracy nad łącznością bluetooth

---
+++ app/src/main/java/com/kaju/fingerprintlockcontrol/CheckForBtActivity.java
@@ -0,0 +1,14 @@
+package com.kaju.fingerprintlockcontrol;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CheckForBtActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_check_for_bt);
+    }
+}
